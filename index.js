ae31d60723dffe32ff0660228e0e81e1106de11e
Sam Jones <samueldjones24@gmail.com>
2019-06-05 20:24:54 +0100

    Initial commit - Project setup

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = require('./src/app');
 
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
   app.listen(3000);
+  console.log('Server is listening on port 3000.')
 });
